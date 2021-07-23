1bdc61a93ef4d7b84c263edab2cd01640c613bc2
“SergiyMyronov” <sergemk@entecheco.com>
2021-07-23 17:47:17 +0300

    trying to use styles

--- classwork/urls.py
+++ classwork/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/', views.PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/new', views.PostCreateView.as_view(), name='post_form'),
-    path('post/edit/<int:pk>', views.PostUpdateView.as_view(), name='post_form'),
-    path('post/delete/<int:pk>', views.PostDeleteView.as_view()),
+    path('userpost/', views.UserPostListView.as_view(), name='user_post_list'),
+    path('userpost/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('userpost/new', views.PostCreateView.as_view(), name='post_form'),
+    path('userpost/edit/<int:pk>', views.PostUpdateView.as_view(), name='post_form'),
+    path('userpost/delete/<int:pk>', views.PostDeleteView.as_view()),
     path('login/', views.login, name='login'),
 ]
--- classwork/views.py
+++ classwork/views.py
@@ -16,12 +16,23 @@ def login(request):
     return HttpResponse("Log in through the admin panel")
 
 
-class PostListView(LoginRequiredMixin, ListView):
-    login_url = reverse_lazy('login')
+class PostListView(ListView):
     model = Post
     fields = ['image', 'header', 'short_description', 'description', 'is_active']
-    success_url = reverse_lazy('post_list')
-    paginate_by = 20
+    paginate_by = 4
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(is_active=True)
+
+
+class UserPostListView(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
+    model = Post
+    template_name = 'classwork/user_post_list.html'
+    fields = ['image', 'header', 'short_description', 'description', 'is_active']
+    success_url = reverse_lazy('user_post_list')
+    paginate_by = 4
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -39,7 +50,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('login')
     model = Post
     fields = ['image', 'header', 'short_description', 'description', 'is_active']
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('user_post_list')
 
     def form_valid(self, form):
         post = form.save(commit=False)
@@ -53,10 +64,10 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
     model = Post
     fields = ['image', 'header', 'short_description', 'description', 'is_active']
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('user_post_list')
 
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     login_url = reverse_lazy('login')
     model = Post
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('user_post_list')
